0c73435d5dde8bb68d8ac8cd8fbe42e81c749a10
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-09-06 13:21:58 -0300

    Create project

---
+++ Marley Spoon/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  Marley Spoon
+//
+//  Created by Tulio de Oliveira Parreiras on 06/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
+
---
+++ Marley Spoon/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Marley Spoon
+//
+//  Created by Tulio de Oliveira Parreiras on 06/09/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
