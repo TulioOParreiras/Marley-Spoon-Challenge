2aa8a2fff725508e5d75feb170c0fa361e8ec8b0
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-09-06 15:49:04 -0300

    Move `RecipeDetailsViewController` to production

--- Marley SpoonTests/Recipe Details/RecipeDetailsUIIntegrationTests.swift
+++ Marley SpoonTests/Recipe Details/RecipeDetailsUIIntegrationTests.swift
@@ -8,49 +8,6 @@
 import XCTest
 import Marley_Spoon
 
-final class RecipeDetailsViewController: UIViewController {
-    
-    public let imageView = UIImageView()
-    public let titleLabel = UILabel()
-    public let caloriesLabel = UILabel()
-    public let chefLabel = UILabel()
-    public let tagsLabel = UILabel()
-    public let descriptionLabel = UILabel()
-    public let spinner = UIActivityIndicatorView()
-    
-    var model: RecipeModel?
-    var imageLoader: ImageDataLoader?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Recipe Details"
-        
-        spinner.startAnimating()
-        if let imageId = model?.imageId {
-            imageLoader?.loadImageData(forImageId: imageId, completion: { result in
-                self.spinner.stopAnimating()
-                self.imageView.image = try? result.get()
-            })
-        }
-        
-        guard let model = model else { return }
-        titleLabel.text = model.title
-        caloriesLabel.text = "Calories: \(model.calories)"
-        chefLabel.text = "Chef: " + (model.chefName ?? "-")
-        tagsLabel.text = tagsText(fromTags: model.tags)
-        descriptionLabel.text = model.description
-    }
-    
-    private func tagsText(fromTags tags: [String]?) -> String {
-        guard let tags = tags, !tags.isEmpty else { return "Tags: -" }
-        var tagsText = tags.reduce("", { return $0 + $1 + ", " })
-        tagsText.removeLast()
-        tagsText.removeLast()
-        return "Tags: " + tagsText
-    }
-    
-}
-
 final class RecipeDetailsUIIntegrationTests: XCTestCase {
     
     func test_view_hasTitle() {
