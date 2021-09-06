2ecf1ddd535423873dac3aac7e2e4eb616b10496
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-09-06 13:37:35 -0300

    Recipes Controller requests recipes from loader

--- Marley SpoonTests/Recipes List/RecipesListsUIIntegrationTests.swift
+++ Marley SpoonTests/Recipes List/RecipesListsUIIntegrationTests.swift
@@ -9,13 +9,35 @@ import XCTest
 
 import UIKit
 
-final class RecipesListViewController: UIViewController {
+final class RecipesListViewController: UITableViewController {
+    
+    var recipesLoader: RecipesListLoader?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
         title = "Recipes List"
+        refresh()
     }
     
+    @objc
+    private func refresh() {
+        recipesLoader?.load()
+    }
+    
+}
+
+protocol RecipesListLoader {
+    func load()
+}
+
+final class LoaderSpy: RecipesListLoader {
+    var loadRecipesCallCount: Int = 0
+    
+    func load() {
+        loadRecipesCallCount += 1
+    }
 }
 
 class RecipesListsUIIntegrationTests: XCTestCase {
@@ -26,5 +48,28 @@ class RecipesListsUIIntegrationTests: XCTestCase {
         sut.loadViewIfNeeded()
         XCTAssertEqual(sut.title, "Recipes List")
     }
+    
+    func test_loadRecipesActions_requestRecipesFromLoader() {
+        let loader = LoaderSpy()
+        let sut = RecipesListViewController()
+        sut.recipesLoader = loader
+        
+        XCTAssertEqual(loader.loadRecipesCallCount, 0)
+        
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadRecipesCallCount, 1)
+        
+        sut.simulateUserInitiatedRecipesReload()
+        XCTAssertEqual(loader.loadRecipesCallCount, 2)
+        
+        sut.simulateUserInitiatedRecipesReload()
+        XCTAssertEqual(loader.loadRecipesCallCount, 3)
+    }
 
+}
+
+extension RecipesListViewController {
+    func simulateUserInitiatedRecipesReload() {
+        refreshControl?.sendActions(for: .valueChanged)
+    }
 }
